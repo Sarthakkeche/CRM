ef660a5657e174633a3ebb3b8e8c76829c33a37f
sarthakkeche <kechesarthak412@gmail.com>
2025-09-11 11:51:56 +0530

    deployed link update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Your backend API URL
+  baseURL: 'https://crm-88d6.onrender.com/api', 
   headers: {
     'Content-Type': 'application/json',
   },
