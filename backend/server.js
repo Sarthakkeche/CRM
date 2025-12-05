34383529f45146247157ac5ad4f4d3c8a38a6b79
sarthakkeche <kechesarthak412@gmail.com>
2025-12-05 10:25:02 +0530

    server3w333

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,10 @@ app.get('/', (req, res) => res.send('API is running...'));
 // --- Define Routes ---
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/customers', require('./routes/customers'));
-app.use('/api/analytics', require('./routes/analytics'));
+
 // In server.js
 app.use('/api/leads', require('./routes/leads'));
+app.use('/api/analytics', require('./routes/analytics'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
